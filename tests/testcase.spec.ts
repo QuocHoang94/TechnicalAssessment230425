2f1acfec1dfc909ed6f42dbe605da868b4b2673f
QuocHoang94 <39055816+QuocHoang94@users.noreply.github.com>
2025-04-23 08:37:01 +0700

    code 230425

--- tests/testcase.spec.ts
+++ tests/testcase.spec.ts
@@ -1,17 +1,14 @@
-import { test } from '@playwright/test';
-import { PracticeFormPage } from '../pages/PracticeForm-page';
+import { test } from '../tests/fixtures'
 import { setup } from './testsetup';
 
 setup();
 
-test('Verify successful form submission with all required fields filled (Happy flow)', async ({ page }) => {
-  const practiceFormPage = new PracticeFormPage(page);
+test('Verify successful form submission with all required fields filled (Happy flow)', async ({ practiceFormPage }) => {
   await practiceFormPage.fillPracticeForm(1);
   await practiceFormPage.verifySubmitPracticeFormSuccess();
 });
 
-test('Verify display error when Mobile Number is missing (Unhappy Flow)', async ({ page }) => {
-  const practiceFormPage = new PracticeFormPage(page);
+test('Verify display error when Mobile Number is missing (Unhappy Flow)', async ({ practiceFormPage }) => {
   await practiceFormPage.fillPracticeForm(0);
   await practiceFormPage.verifySubmitPracticeFormUnSuccess();
 });
